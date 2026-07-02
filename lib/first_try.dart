eafdf4148cdc02327eb1f4f09f160ee017bb1941
Sena Yildiz <sena.yildiz@mera.com>
2026-07-02 10:57:48 +0300

    feat(welcome): template temizlendi ve welcome ekrani eklendi

---
+++ lib/first_try.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FirstTry extends StatelessWidget {
+  const FirstTry({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Merhaba Antigravity!')));
+  }
+}
